aeace28aa1506974c28a00bcf4f00a5e8f6f9c9a
David <dvtuazon@gmail.com>
2021-03-12 01:45:22 -0800

    responsive

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -4,7 +4,7 @@ import { Container, Col } from 'reactstrap';
 function About() {
     return (
         <Container>
-            <Col><h1>About Me</h1></Col>
+            <Col><h1 className="title">About Me</h1></Col>
             <Col>
                 <p>
                     
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -10,36 +10,44 @@ function Header() {
     return (
         <>
             <div className="jumbotron jumbotron-fluid typewriter">
-                <h1 className="name typing-erase">
+                <h1 className="name typing-erase pl-2">
                     <span style={{ color: 'rgb(64, 130, 161)' }}>const</span>
-                    <span style={{ color: 'rgb(46, 92, 114)' }}> name</span> <span style={{ color: 'white' }}> =</span>
-                    <span style={{ color: 'rgb(179, 116, 74)' }}> 'David Tuazon'</span>
+                    <span style={{ color: 'rgb(46, 92, 114)' }}> name</span> <span style={{ color: 'rgb(219, 219, 219);' }}> =</span>
+                    <span style={{ color: 'rgb(179, 116, 74)' }}> 'David Tuazon'</span><span style={{ color: 'rgb(219, 219, 219)' }}>;</span>
                 </h1>
             </div>
             <Container fluid>
                 <Navbar sticky="top">
                     <Nav navbar className="header">
                         <Container>
-                            <NavItem>
-                                <NavLink className="nav-link header-item" href="/projects">
-                                    &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Projects</span> /&#62;
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link header-item" href="/about">
-                                    &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>About</span> /&#62;
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link header-item" href="/contact">
-                                    &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Contact</span> /&#62;
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link header-item" href="/resume">
-                                    &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Resume</span> /&#62;
-                                </NavLink>
-                            </NavItem>
+                            <Col xs="3">
+                                <NavItem>
+                                    <NavLink className="nav-link header-item" href="/projects">
+                                        &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Projects</span> /&#62;
+                                    </NavLink>
+                                </NavItem>
+                            </Col>
+                            <Col xs="3">
+                                <NavItem>
+                                    <NavLink className="nav-link header-item" href="/about">
+                                        &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>About</span> /&#62;
+                                    </NavLink>
+                                </NavItem>
+                            </Col>
+                            <Col xs="3">
+                                <NavItem>
+                                    <NavLink className="nav-link header-item" href="/contact">
+                                        &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Contact</span> /&#62;
+                                    </NavLink>
+                                </NavItem>
+                            </Col>
+                            <Col xs="3">
+                                <NavItem>
+                                    <NavLink className="nav-link header-item" href="/resume">
+                                        &#60;<span style={{ color: 'rgb(54, 190, 150)'}}>Resume</span> /&#62;
+                                    </NavLink>
+                                </NavItem>
+                            </Col>
                         </Container>
                     </Nav>
                 </Navbar>
