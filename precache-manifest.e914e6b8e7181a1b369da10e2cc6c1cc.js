3f56c1151afee2522ead927872131055d3b61115
David <dvtuazon@gmail.com>
2021-04-06 05:58:57 -0700

    Updates

---
+++ precache-manifest.e914e6b8e7181a1b369da10e2cc6c1cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855a17653b88d0ee4a49d7223126272d",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "ef02f447fde25e04ead3",
+    "url": "/portfoliowebsite/static/css/2.23c9d44f.chunk.css"
+  },
+  {
+    "revision": "384950386b6f795e856d",
+    "url": "/portfoliowebsite/static/css/main.e50bb9fc.chunk.css"
+  },
+  {
+    "revision": "ef02f447fde25e04ead3",
+    "url": "/portfoliowebsite/static/js/2.5a932062.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfoliowebsite/static/js/2.5a932062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384950386b6f795e856d",
+    "url": "/portfoliowebsite/static/js/main.58654558.chunk.js"
+  },
+  {
+    "revision": "864ab8c372712cf18cd7",
+    "url": "/portfoliowebsite/static/js/runtime-main.4c0d82cc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfoliowebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfoliowebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfoliowebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfoliowebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfoliowebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7da1e25dea100fc634ffc5b4c04ee5d4",
+    "url": "/portfoliowebsite/static/media/profilepic.7da1e25d.jpg"
+  }
+]);
