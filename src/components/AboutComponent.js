1a9b0c0fc9bd93ddea1061b210f44125e5d16c7a
David <dvtuazon@gmail.com>
2021-04-19 18:15:16 -0700

    fixes

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -8,14 +8,14 @@ function About() {
             <Container className="container-2">
                 <div className="about-text">
                     <h1 className="title col-xs-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 text-center">About Me</h1>
-                    <p className="mx-3">
+                    <p className="px-3">
                         My name is David Tuazon, and I am a front end developer. I
                         use JavaScript-based front end technologies such as React and React
                         Native, and I am also familiar with Node.js, Express, and MongoDB,
                         allowing me to create a back end as well.
                         </p>
                     <br />
-                    <p className="mx-3">
+                    <p className="px-3">
                         I began teaching myself web development languages on Udemy after being 
                         introduced to them in high school. I learned HTML and CSS, but I felt 
                         that I wanted a good structure for learning programming, so I decided 
@@ -23,7 +23,7 @@ function About() {
                         and the MERN stack.
                         </p>
                     <br />
-                    <p className="mx-3">
+                    <p className="px-3">
                         I am good at picking up new concepts, identifying patterns, and integrating it 
                         into my work. As a naturally curious person, I am a believer of lifelong learning
                         and will always want to expand my skillset. My goal is to become flexible engineer
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -13,14 +13,20 @@ function Home() {
                         <img src={profilepic} className="pic mx-auto" alt="profile" />
                     </Col>
                     <Col xs="12" md="8" className="center-home home-text text-center">
-                        <h1 className="big-text">Front End Engineer</h1>
+                        <h1 className="big-text pt-5">Front End Engineer</h1>
                         <p>
                             Hello there! I'm David. <br /> I use React and other web technologies.
                         </p>
                         <Row className="mt-5 mb-5">
-                            <Link to="projects" className="btn btn-secondary no-wrap" smooth offset={-79}>See my work &rarr;</Link>
-                            <p className="mx-3">or</p>
-                            <Link to="contact" className="btn btn-secondary no-wrap" smooth offset={-89}>Contact me &rarr;</Link>
+                            <Col xs="12" lg="5">
+                                <Link to="projects" className="btn btn-secondary no-wrap" smooth offset={-79}>My Work &rarr;</Link>
+                            </Col>
+                            <Col xs="12" lg="2">
+                                <p className="my-1">or</p>
+                            </Col>
+                            <Col xs="12" lg="5">
+                                <Link to="contact" className="btn btn-secondary no-wrap" smooth offset={-89}>Contact Me &rarr;</Link>
+                            </Col>
                         </Row>
                     </Col>
                 </Row>
